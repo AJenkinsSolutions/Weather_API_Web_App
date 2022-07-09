ddf5a8896bbba1ef2521867cb295d584ce408d04
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-07-09 17:16:07 -0500

    Add user input, Add user query to Api call

--- app.js
+++ app.js
@@ -10,14 +10,7 @@ app.use(express.urlencoded({extended:true}));
 const https = require('node:https');
 const { rawListeners } = require('node:process');
 
-//API Endpoints, Path, Parameters
-const endpoint = 'https://api.openweathermap.org/data/2.5/weather'
-const apiKey= '7dd1119e4ccd073552353bdfd941b95f'
-let lat = 29.951065
-let lon = -90.071533
-const units =  'metric'
-//Endpoint
-let url = endpoint + '?lat='+ lat + '&lon=' + lon + '&units=' + units + '&appid=' + apiKey
+
 
 
 
@@ -31,41 +24,60 @@ app.listen(port,() =>{
 
 app.get('/', (req, res)=>{
 
+   
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.post('/', (req, res) => {
+    console.log('Server: Post Received')
+
+    //user city query
+    const city = req.body.cityName;
+    //API Endpoints, Path, Parameters
+    const endpoint = 'https://api.openweathermap.org/data/2.5/weather'
+    const apiKey= '7dd1119e4ccd073552353bdfd941b95f'
+    const units =  'metric'
+    //Endpoint
+    let url = endpoint +'?q='+ city +'&units=' + units + '&appid=' + apiKey
+
+    //Api Calling 
     https.get(url, (response)=>{
         for(let i=200; i<=300; i++){
             if(i == response.statusCode){
                 console.log(response.statusCode)
                 response.on('data',(data)=>{
+                    
                     //Parse Our Jsoin data to Js Object
                     let jObject = JSON.parse(data);
-
+    
                     const temp = jObject.main.temp;
-                    const location = jObject.name;
                     const humidity = jObject.main.humidity;
                     const description = jObject.weather[0].description;
                     
                     //Weather icon
                     const icon = jObject.weather[0].icon;
                     let imgUrl = 'http://openweathermap.org/img/wn/'+ icon +'@2x.png'
-
-
+    
+    
                     // console.log('City: '+location);
                     // console.log('Temp: ' + temp);
                     // console.log('Humidity: '+ humidity);
                     // console.log('Description: '+ description);
-
-
-                    res.write('<h1>' + 'The Temputre in ' + location + ' is ' + temp + ' degress with ' + humidity + '% humidity'+ '</h1>')
+    
+    
+                    res.write('<h1>' + 'The Temputre in ' + city + ' is ' + temp + ' degress with ' + humidity + '% humidity'+ '</h1>')
                     res.write('<h2>'+'The weather is '+ description + '</h2>')
                     res.write('<img src=' + imgUrl + '>')
-                    res.send() 
-
                     
+    
+                
                 })
             }
         }
-
+    
     })
     
-
 })
+
+
